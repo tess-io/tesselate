8f4bc8551c85196d509bad9c14b637bee8d87289
whereyourspace <whereyourspace@gmail.com>
2025-09-22 03:59:28 +0300

    [tf][pve_pool] Review

--- modules/pve_pool/outputs.tf
+++ modules/pve_pool/outputs.tf
@@ -1,4 +1,7 @@
 output "ip_addresses" {
   description = "List of IP addresses of corresponding VM from the pool"
-  value       = [ for ind, vm in proxmox_virtual_environment_vm.vm: vm.agent[0].enabled ? vm.ipv4_addresses[1][0] : local.ipv4_addresses[ind] ]
+  value       = [
+    for ind, vm in proxmox_virtual_environment_vm.vm:
+      vm.agent[0].enabled ? vm.ipv4_addresses[1][0] : local.ipv4_addresses[ind]
+  ]
 }
