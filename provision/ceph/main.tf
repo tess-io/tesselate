872171a05a972fa19f5b6b5e667c36b0bf5ab3e6
whereyourspace <whereyourspace@gmail.com>
2025-11-16 07:39:18 +0300

    [provision][ceph] group vms

--- provision/ceph/main.tf
+++ provision/ceph/main.tf
@@ -23,19 +23,85 @@ provider "proxmox" {
   password = local.proxmox_pass
 }
 
+locals {
+  vms = {
+    mon = {
+      size = 3
+
+      resources = {
+        memory = 5
+      }
+
+      disks = {
+        other = [
+          { storage = "SSD", size = 100, ssd = true },
+          { storage = "SSD", size = 8, ssd = true },
+        ]
+      }
+
+      acls = [
+        { cidr = "0.0.0.0/0", ports = "0:65535", policy = "accept", proto = "tcp" }
+      ]
+    }
+
+    mgr = {
+      size = 1
+
+      resources = {
+        cpu = 2
+      }
+
+      disks = {
+        other = [ { storage = "SSD", size = 8, ssd = true } ]
+      }
+
+      acls = [
+        { cidr = "0.0.0.0/0", ports = "0:65535", policy = "accept", proto = "tcp" }
+      ]
+    }
+
+    osd = {
+      size = 1
+
+      resources = {
+        cpu = 4
+        memory = 4
+      }
+
+      disks = {
+        other = [
+          { storage = "SSD", size = 640, ssd = true },
+          { storage = "HDD", size = 3072 },
+        ]
+      }
+
+      acls = [
+        { cidr = "0.0.0.0/0", ports = "0:65535", policy = "accept", proto = "tcp" }
+      ]
+    }
+  }
+
+  start_id = 121
+  vms_size = [ for name, props in local.vms: lookup(props, "reserve", 5) > props.size ? lookup(props, "reserve", 5) : props.size ]
+  vms_start_id = {
+    for ind, key in keys(local.vms):
+      key => local.start_id + sum(ind > 0 ? slice(local.vms_size, 0, ind) : [0])
+  }
+}
 
 module "ceph" {
   source = "../../modules/pve_pool"
 
-  name        = "ceph-ha"
-  vms_name    = "ceph-srv"
-  description = "Ceph cluster VMs pool"
-  tags        = ["ceph", "production"]
-  start_id    = 121
+  for_each = local.vms
+  name        = "Ceph-${upper(each.key)}"
+  vms_name    = "ceph-${each.key}-srv"
+  description = "Ceph ${upper(each.key)} VMs pool"
+  tags        = ["ceph", "ceph-${each.key}", "production"]
+  start_id    = local.vms_start_id[each.key]
 
   agent_on = true
   node     = "pve"
-  size     = 1
+  size     = each.value.size
 
   auth = {
     user     = "user"
@@ -43,11 +109,11 @@ module "ceph" {
     ssh_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0yO9RABzbP4OhuNYjjAo+xtwyVUHsg9sbIQxhYIFMp"]
   }
 
-  resources = {
-    cpu    = 8
-    memory = 32
+  resources = merge({
+    cpu    = 2
+    memory = 4
     hugepg = false
-  }
+  }, lookup(each.value, "resources", {}))
 
   base = {
     os      = "ubuntu"
@@ -60,17 +126,12 @@ module "ceph" {
     dns    = [ "8.8.8.8" ]
     domain = "local"
 
-    acls = [
-      { cidr = "0.0.0.0", ports = "22,80,443", policy = "accept", proto = "tcp", },
-    ]
+    acls = lookup(each.value, "acls", [])
   }
 
-  disks = {
-    root      = { storage = "SSD", size = 8 },
-    cloudinit = { storage = "SSD" },
-    other = [
-      { storage = "SSD", size = 428, },
-      { storage = "HDD", size = 2048 }
-    ],
-  }
+  disks = merge({
+    root      = { storage = "SSD", size = 8 }
+    cloudinit = { storage = "SSD" }
+    other = [ { storage = "SSD", size = 8, ssd = true } ]
+  }, lookup(each.value, "disks", {}))
 }
